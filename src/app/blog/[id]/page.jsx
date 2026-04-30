243b8b23b6f3d507538fe87dda6e828ddf096f54
Ivan Filippenko <sevcenkoivan395@gmail.com>
2026-04-30 20:39:16 +0300

    made SSG generation for posts

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,22 +1,32 @@
-import { fetchPostFromDBById } from '../../../lib/db'
+import { fetchPostFromDBById, fetchPostsFromDB } from '../../../lib/db'
 
 
 export async function generateMetadata({ params }) {
     const { id } = await params
-    const {title} = await fetchPostFromDBById(id)
+    const { title } = await fetchPostFromDBById(id)
 
     return {
         title: title
     }
 }
 
+
+    export async function generateStaticParams() {
+        const posts = await fetchPostsFromDB()
+
+        return posts.map(post => ({
+            slug: post.id.toString()
+        }))
+    }
+
+
 const Post = async ({ params }) => {
     const { id } = await params
     const post = await fetchPostFromDBById(id)
     return (
         <div className="post-container">
-           <h1>{post.title}</h1>
-           <p>{post.body}</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
         </div>
     )
 }
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,30 +1,31 @@
 
-        import { fetchPostsFromDB, getPostsFromDBByKeyword, fetchCountPostFromDB, fetchCountPostFromDBByKeyword } from '../../lib/db'
-        import SearchPost from '../../components/SearchPost/SearchPost'
-        import PostsList from '../../components/PostsList/PostsList'
+import { fetchPostsFromDB, getPostsFromDBByKeyword, fetchCountPostFromDB, fetchCountPostFromDBByKeyword } from '../../lib/db'
+import SearchPost from '../../components/SearchPost/SearchPost'
+import PostsList from '../../components/PostsList/PostsList'
 
-        export const metadata = {
-            title: "blog",
-        }
+export const metadata = {
+    title: "blog",
+}
 
-        const Blog = async ({ searchParams }) => {
 
-            const { search = '' } = await searchParams
-            const keyword = search
+const Blog = async ({ searchParams }) => {
 
-            const posts = keyword ? await getPostsFromDBByKeyword(keyword) : await fetchPostsFromDB()
-            const countPosts = keyword ? await fetchCountPostFromDBByKeyword(keyword) : await fetchCountPostFromDB()
-            const infoWord = keyword ? 'posts found:' : 'total posts: '
+    const { search = '' } = await searchParams
+    const keyword = search
 
-            return (
-                <div className='blog-container'>
-                    <h1>BLOG</h1>
+    const posts = keyword ? await getPostsFromDBByKeyword(keyword) : await fetchPostsFromDB()
+    const countPosts = keyword ? await fetchCountPostFromDBByKeyword(keyword) : await fetchCountPostFromDB()
+    const infoWord = keyword ? 'posts found:' : 'total posts: '
 
-                    <SearchPost />
-                    <h2 className='info-word'>  {infoWord} {countPosts} </h2>
-                    <PostsList initialPosts={posts} keyword={keyword} />
-                </div>
-            )
-        }
+    return (
+        <div className='blog-container'>
+            <h1>BLOG</h1>
 
-        export default Blog
+            <SearchPost />
+            <h2 className='info-word'>  {infoWord} {countPosts} </h2>
+            <PostsList initialPosts={posts} keyword={keyword} />
+        </div>
+    )
+}
+
+export default Blog
