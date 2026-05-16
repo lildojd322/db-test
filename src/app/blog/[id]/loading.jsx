c1feacbcb96ea49b4f753de124dc2cfa94e8f984
Ivan Filippenko <sevcenkoivan395@gmail.com>
2026-05-16 17:32:19 +0300

    A skeleton for the post's internals and a session check when accessing /signin

--- src/app/blog/[id]/loading.jsx
+++ src/app/blog/[id]/loading.jsx
@@ -1,9 +1,33 @@
-
+import styles from '../../../components/Post/Post.module.scss'
 
 const LoadingPost = () => {
     return (
-        <div >
-           Loading...
+        <div className={styles.postContainer}>
+        
+            <h1 className={styles.title}>
+                <div className="skeleton-line short" style={{marginBottom: '2rem', width: '500px', height: '30px'}}></div>
+            </h1>
+
+            <div className={styles.body}>
+                <div className="skeleton-line"></div>
+
+                <div className="skeleton-line short"></div>
+            </div>
+            <div className={styles.authorLink}>
+                <pre className={styles.authorName}>
+                    <div className='skeleton-line short' style={{width: '100px'}}></div>
+                </pre>
+                <div className={styles.avatar} style={{
+                    background: '#6b7280',
+                    marginRight: '20px',
+                    animation: 'pulse 1.5s ease-in-out infinite'
+                }}></div>
+            </div>
+
+           
+            <pre className={styles.meta}>
+                <div className='skeleton-line short' style={{width: '260px'}}></div>
+            </pre>
         </div>
     )
 }
--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,22 +1,29 @@
 import Link from "next/link"
 import GoogleButton from "../../components/GoogleButton/GoogleButton"
+import { authConfig } from '../../lib/auth'
 import SignInForm from '../../components/SigninForm/SignInForm'
-const Signin = () => {
+import { getServerSession } from "next-auth/next"
+import { redirect } from 'next/navigation'
+
+const Signin = async () => {
+    const session = await getServerSession(authConfig)
+
+    if (session) {
+        redirect("/profile")
+    }
+
     return (
-        <div className="authorizeContainer" >
+        <div className="authorizeContainer">
             <GoogleButton />
-            <div >
+            <div>
                 <p className="signOr">or sign in with email</p>
             </div>
             <SignInForm />
             <div className="authFooter">
                 <p>
-                    Don't have an account?   <Link href='/register'>Register right now</Link>
+                    Don't have an account? <Link href='/register'>Register right now</Link>
                 </p>
             </div>
-
-
-
         </div>
     )
 }
