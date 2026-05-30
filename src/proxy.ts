284b4be49e968929760d2774ce4c1d234cd06593
Ivan Filippenko <sevcenkoivan395@gmail.com>
2026-05-30 14:35:58 +0300

    second layer of session verification using proxy (middleware)

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,33 @@
-export { default as proxy } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 
-export const config = { matcher: ['/profile', '/protected/:path*'] }
+export const config = { matcher: ['/profile', '/protected/:path*', '/signin', '/register'] }
+
+export function proxy(request: NextRequest) {
+
+    let sessionToken = request.cookies.get('__Secure-next-auth.session-token') ||
+        request.cookies.get('next-auth.session-token')
+
+
+    const { pathname } = request.nextUrl
+
+
+    if (sessionToken) {
+        if (pathname === '/signin' || pathname === '/register') {
+
+            return NextResponse.redirect(new URL('/profile', request.url))
+        }
+
+        return NextResponse.next()
+    }
+    if (!sessionToken) {
+        if (pathname === '/profile') {
+            return NextResponse.redirect(new URL('/signin', request.url))
+        }
+
+    }
+
+
+    return NextResponse.next()
+}
